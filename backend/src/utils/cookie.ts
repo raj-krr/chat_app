042ffc4bdc53f50fe2867185912741b82a9151b7
raj-krr <mr.rajkumar3579@gmail.com>
2026-01-23 14:18:13 +0530

    refactor cookie options: unify secure and sameSite logic for consistency

--- backend/src/utils/cookie.ts
+++ backend/src/utils/cookie.ts
@@ -1,23 +1,26 @@
 import { CookieOptions } from "express";
 
-const isProduction = process.env.NODE_ENV === "production";
+const isSecureCookie =
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "development";
+
 
 export const RefreshOptions: CookieOptions = {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
+    secure: isSecureCookie,
+    sameSite: isSecureCookie ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 export const AccessOptions: CookieOptions = {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
+    secure:isSecureCookie,
+    sameSite: isSecureCookie? "none" : "lax",
     maxAge:    30 * 60 * 1000,
 };
 
 export const logoutOptions: CookieOptions = {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
+    secure: isSecureCookie,
+    sameSite: isSecureCookie ? "none" : "lax",
     expires: new Date(0),
 };
